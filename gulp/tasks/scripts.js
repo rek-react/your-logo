aa89ef14c1a622037938af9c856755769ac6d0f8
Виктор <viktorhelp12@gmail.com>
2022-09-07 09:35:12 +0300

    Added babel-loader

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -4,6 +4,25 @@ export const scripts = () => {
     return app.gulp.src(app.path.src.scripts, { sourcemap: app.isDev })
         .pipe(webpack({
             mode: app.isDev ? 'development' : 'production',
+
+            module: {
+                rules: [
+                    {
+                        test: /\.m?js$/,
+                        exclude: /(node_modules|bower_components)/,
+                        resolve: {
+                            fullySpecified: false
+                        },
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['@babel/preset-env'],
+                                plugins: ['@babel/plugin-proposal-object-rest-spread']
+                            }
+                        }
+                    }
+                ]
+            },
             output: {
                 filename: 'script.min.js'
             }
